79a370f0db2cd2e1d64f99439694069cee226210
AndrePort <aporto-@hotmail.com>
2023-01-04 17:36:18 -0300

    feat: implement add item with api

--- assets/pages/home/index.js
+++ assets/pages/home/index.js
@@ -72,6 +72,13 @@ const renderFinanceElements = (data) => {
 
   // render total items
   const financeCard1 = document.getElementById("finance-car-1");
+  financeCard1.innerHTML = "";
+
+  const totalSubtext = document.createTextNode("Total de lançamentos");
+  const totalSubtextElement = document.createElement("h3");
+  totalSubtextElement.appendChild(totalSubtext);
+  financeCard1.appendChild(totalSubtextElement);
+
   const totalText = document.createTextNode(totalItems);
   const totalElement = document.createElement("h1");
   totalElement.className = "mt smaller";
@@ -80,6 +87,13 @@ const renderFinanceElements = (data) => {
 
     // render revenue
   const financeCard2 = document.getElementById("finance-car-2");
+  financeCard2.innerHTML = "";
+
+  const revenueSubtext = document.createTextNode("Receitas");
+  const revenueSubtextElement = document.createElement("h3");
+  revenueSubtextElement.appendChild(revenueSubtext);
+  financeCard2.appendChild(revenueSubtextElement);
+
   const revenueText = document.createTextNode(
       new Intl.NumberFormat('pt-BR', {
       style: 'currency',
@@ -93,6 +107,13 @@ const renderFinanceElements = (data) => {
 
   // render expenses
   const financeCard3 = document.getElementById("finance-car-3");
+  financeCard3.innerHTML = "";
+
+  const expensesSubtext = document.createTextNode("Despesas");
+  const expensesSubtextElement = document.createElement("h3");
+  expensesSubtextElement.appendChild(expensesSubtext);
+  financeCard3.appendChild(expensesSubtextElement);
+
   const expensesText = document.createTextNode(
       new Intl.NumberFormat('pt-BR', {
       style: 'currency',
@@ -106,6 +127,13 @@ const renderFinanceElements = (data) => {
 
   // render balance
   const financeCard4 = document.getElementById("finance-car-4");
+  financeCard4.innerHTML = "";
+
+  const balanceSubtext = document.createTextNode("Balanço");
+  const balanceSubtextElement = document.createElement("h3");
+  balanceSubtextElement.appendChild(balanceSubtext);
+  financeCard4.appendChild(balanceSubtextElement);
+
   const balanceText = document.createTextNode(
       new Intl.NumberFormat('pt-BR', {
       style: 'currency',
@@ -167,9 +195,94 @@ const onLoadUserInfo = () => {
   const nameText = document.createTextNode(name.charAt(0));
   nameElement.appendChild(nameText);
   navbarUserAvatar.appendChild(nameElement);
+};
+
+const onLoadCategories = async () => {
+  try {
+    const categoriesSelect = document.getElementById("input-category");
+    const response = await fetch(
+      "https://mp-wallet-app-api.herokuapp.com/categories"
+    );
+    const categoriesResult = await response.json();
+    categoriesResult.map((category) => {
+      const option = document.createElement("option");
+      const categoryText = document.createTextNode(category.name);
+      option.id = `category_${category.id}`;
+      option.value = category.id;
+      option.appendChild(categoryText);
+      categoriesSelect.append(option);
+    });
+  } catch (error) {
+    alert("Error ao carregar categorias")
+  }
 }
+
+const onOpenModal = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "flex";
+};
+
+const onCloseModal = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+};
+
+const onCallAddFinance = async (data) => {
+  try {
+    const email = localStorage.getItem("@WalletApp:userEmail");
+
+    const response = await fetch(
+      "https://mp-wallet-app-api.herokuapp.com/finances",
+      {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-type": "application/json",
+          email: email,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+  } catch (error) {
+    return { error }
+  }
+};
+
+
+const onCreateFinanceRelease = async (target) => {
+  try {
+    const title =  target[0].value;
+    const value =  Number(target[1].value);
+    const date =  target[2].value;
+    const category =  Number(target[3].value);
+    const result = await onCallAddFinance({
+      title,
+      value,
+      date,
+      category_id: category,
+    });
+
+    if (result.error) {
+      alert("Erro ao adicionar novo dado financeiro.");
+      return;
+    }
+    onCloseModal();
+    onLoadFinancesData();
+  } catch (error) {
+    alert("Erro ao adicionar novo dado financeiro.");
+  }
+};
 
 window.onload = () => {
   onLoadUserInfo();
   onLoadFinancesData();
-}
+  onLoadCategories();
+
+  const form = document.getElementById("form-finance-release");
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    onCreateFinanceRelease(event.target);
+  }
+};
